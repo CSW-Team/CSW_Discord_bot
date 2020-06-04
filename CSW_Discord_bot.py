603ebabf23fdccf0f53007565634c09f8e86b7d1
ChanWooLee <60313224+Blakefive@users.noreply.github.com>
2020-06-04 13:37:35 +0900

    Update CSW_Discord_bot.py

--- CSW_Discord_bot.py
+++ CSW_Discord_bot.py
@@ -1,4 +1,5 @@
 import discord
+import time
 
 def gotest(list1):
     j = ""
@@ -17,15 +18,36 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    check = 1
+    week = ["월요일","화요일","수요일","목요일","금요일","토요일","일요일"]
     if message.content.startswith("!hello"):
-        j = ""
         f = open('discord data/hello.txt','r')
         h = f.readlines()
-        await message.channel.send(gotest(h))
+        now = time.localtime()
+        n = time.localtime().tm_wday
+        await message.channel.send(gotest(h)+"\n"
+                                   +"```"
+                                   +"```"+"cs"+"\n"
+                                   +"'Today is "+("%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday))+ " " +week[n] + "'"+"\n"
+                                   +"'This time is "+("%02d:%02d:%02d" % (now.tm_hour, now.tm_min, now.tm_sec))
+                                   +"'```")
     if message.content.startswith("!help"):
         f = open('discord data/help.txt','r')
         h = f.readlines()
         await message.channel.send(gotest(h))
+    if message.content.startswith("!time"):
+        now = time.localtime()
+        await message.channel.send("```cs"+"\n"+"'"+("%02d:%02d:%02d" % (now.tm_hour, now.tm_min, now.tm_sec))+"'```")
+    if message.content.startswith("!day"):
+        now = time.localtime()
+        n = time.localtime().tm_wday
+        await message.channel.send("```cs"+"\n"+"'"+(("%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday))+ " " +week[n])+"'```")
+    if message.content.startswith("!now"):
+         now = time.localtime()
+         n = time.localtime().tm_wday
+         await message.channel.send("```cs"+"\n"+"'"+(("%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday))+ " " +week[n])+"'"+"\n"
+                                    +"'"+("%02d:%02d:%02d" % (now.tm_hour, now.tm_min, now.tm_sec))
+                                    +"'```")
     if message.content.startswith("!python"):
         f = open('discord data/python.txt','r')
         h = f.readlines()
@@ -54,6 +76,8 @@ async def on_message(message):
         f = open('discord data/go.txt','r')
         h = f.readlines()
         await message.channel.send(gotest(h))
-        
+
+
+
 client.run(token)
 f.close()
